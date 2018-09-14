34ed22987fd71e2cbc55b5650a5894af6b903a3d
Andy Webster <andy.webster.online@gmail.com>
2018-09-14 12:10:03 +0100

    linked db

--- app_api/config/DB.js
+++ app_api/config/DB.js
@@ -1,4 +1,5 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/job-board',
+    // DB: 'mongodb://localhost:27017/job-board',
+    DB: 'mongodb://<andy>:<123andy>@ds157422.mlab.com:57422/job-board',
     secret: 'MYSECRET'
  };
