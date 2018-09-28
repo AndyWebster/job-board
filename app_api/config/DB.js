ec17919b149bee525b65248fa0b2c5add2698f25
Andy Webster <andy.webster.online@gmail.com>
2018-09-28 14:10:10 +0100

    Env variables

---
+++ app_api/config/DB.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // local development db
+    DB: 'mongodb://localhost:27017/job-board',
+    secret: 'MYSECRET'
+
+    // production db
+    // DB: 'mongodb://andy:123andy@ds157422.mlab.com:57422/job-board',
+ };
