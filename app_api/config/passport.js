6f507c5ef7db598fc0a8ec4656096ee4b9794493
Andy Webster <andy.webster.online@gmail.com>
2018-09-29 10:57:13 +0100

    port variables

--- app_api/config/passport.js
+++ app_api/config/passport.js
@@ -1,7 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/users');
-const secret = process.env.SECRET;
+const secret = process.env.SECRET || SECRET;
 
 module.exports = function(passport){
   let opts = {};
