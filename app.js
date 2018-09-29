5326b5ff8e73f022d9dc27d7c1e7efafe91176f5
Andy Webster <andy.webster.online@gmail.com>
2018-09-29 20:23:10 +0100

    server error fix

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ mongoose.set('useCreateIndex', true);
 // On Connection And Error
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.PROD_MONGODB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected' + process.env.DB_URL_PUBLIC || DB_URL_PUBLIC) },
-  err => { console.log('Can not connect to the database'+ err + "on URL: " + process.env.DB_URL || DB_URL)}
+  () => {console.log('Database is connected' + process.env.PROD_MONGODB) },
+  err => { console.log('Can not connect to the database'+ err)}
 );
 
 const app = express();
