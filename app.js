8b45e237c18320d0bbbfd567143a92c6bdad8c26
Andy Webster <andy.webster.online@gmail.com>
2018-09-29 20:10:18 +0100

    Troubleshoot db

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 mongoose.set('useCreateIndex', true);
 // On Connection And Error
 mongoose.Promise = global.Promise;
-mongoose.connect(PROD_MONGODB || process.env.PROD_MONGODB, { useNewUrlParser: true }).then(
+mongoose.connect(process.env.PROD_MONGODB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected' + process.env.DB_URL_PUBLIC || DB_URL_PUBLIC) },
   err => { console.log('Can not connect to the database'+ err + "on URL: " + process.env.DB_URL || DB_URL)}
 );
